6cb0e1666f3feb7e83a5857c9170e9db9b3334e6
Marcos Antas <marcos.antas@hotmail.com.br>
2022-11-06 00:11:47 -0300

    Implementação do mapeamento de usuario do JSON para a entidade de negócio. O teste não foi implementado completamente.

--- AutomapperTeste/TesteUsuarioNegocioAPI.cs
+++ AutomapperTeste/TesteUsuarioNegocioAPI.cs
@@ -5,20 +5,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
+using ClienteJiraAPI.Modelos.Entrada;
 
 namespace AutomapperTeste
 {
 	public class TesteUsuarioNegocioAPI
 	{
+		public TesteUsuarioNegocioAPI()
+		{
+
+		}
+
 		[Fact]
 		public void Test1()
 		{
-			Usuario origem = new Usuario(7, "marcos.antas@hotmail.com.br", "", "Marcos André", true);
+			MembroEquipe origem = new MembroEquipe()
+			{
+				accountId = 7,
+				displayName = "Marcos André",
+				emailAddress = "marcos.antas@hotmail.com.br",
+				active = true,
+				avatarUrls = new Dictionary<string, string>()
+			};
 
-			origem.Id.Should().Be(7);
-			origem.UrlAvatar.Should().Be(string.Empty);
-			origem.Tarefas.Should().NotBeNull();
-			origem.Tarefas.Should().BeEmpty();
+			Usuario destino = new Usuario(7, "marcos.antas@hotmail.com.br", "", "Marcos André", true);
+
+			destino.Id.Should().Be(7);
+			destino.UrlAvatar.Should().Be(string.Empty);
+			destino.Tarefas.Should().NotBeNull();
+			destino.Tarefas.Should().BeEmpty();
 		}
 	}
 }
--- Repositorio/Profiles/UsuarioProfile.cs
+++ Repositorio/Profiles/UsuarioProfile.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using ClienteJiraAPI.Modelos.Entrada;
+using Negocio.Modelos.Entidades;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Repositorio.Profiles
@@ -9,7 +12,39 @@ namespace Repositorio.Profiles
 	{
 		public UsuarioProfile()
 		{
-
+			CreateMap<MembroEquipe, Usuario>()
+				.ForMember(d => d.Id, opt => opt.MapFrom(o => o.accountId))
+				.ForMember(d => d.Email, opt => opt.MapFrom(o => o.emailAddress))
+				.ForMember(d => d.Nome, opt => opt.MapFrom(o => o.displayName))
+				.ForMember(d => d.UrlAvatar, opt => opt.MapFrom(o => o.avatarUrls[o.avatarUrls.Keys.ToList().First()]))
+				.ForMember(d => d.Ativo, opt => opt.MapFrom(o => o.active));
 		}
 	}
+
+	//public class ItemProfile : Profile
+	//{
+	//	public ItemProfile()
+	//	{
+	//		CreateMap<ItemUpdateModel, ItemInputModel>();
+
+	//		CreateMap<ItemInputModel, Item>()
+	//			.ForMember(d => d.RelacoesUsuarios, opt =>
+	//			{
+	//				opt.Condition(s => s.Relacao != EnumRelacaoUsuarioItem.NaoPossuo);
+	//				opt.MapFrom(s => new[] { new RelacaoItemUsuarioInputModel { Relacao = s.Relacao, IdUsuario = s.IdUsuario, Comentario = s.Comentario } });
+	//			});
+	//		CreateMap<RelacaoItemUsuarioInputModel, ItemUsuario>()
+	//			.ConvertUsing(s => (s.Relacao == EnumRelacaoUsuarioItem.NaoPossuo)
+	//				? null : new ItemUsuario(s.IdUsuario, s.IdItem, (int)s.Relacao, s.Comentario));
+
+	//		CreateMap<Item, ItemViewModel>()
+	//			.IncludeBase<Item, ItemBasicViewModel>();
+	//		CreateMap<Item, ItemBasicViewModel>()
+	//			.IncludeMembers(r => r.RelacoesUsuarios.FirstOrDefault());
+	//		CreateMap<ItemUsuario, ItemBasicViewModel>();
+	//		CreateMap<ItemUsuario, EnumRelacaoUsuarioItem>(MemberList.None)
+	//			.ConvertUsing(src => (EnumRelacaoUsuarioItem)src.Relacao);
+
+	//	}
+	//}
 }
